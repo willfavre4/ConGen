c051483d85a2c679d9a4652de45f61aa1b2e699c
Will Reid <willfavre4@gmail.com>
2016-12-08 10:12:49 -0500

    THE ACTUAL VERSION

--- congen.py
+++ congen.py
@@ -1,51 +1,4 @@
 #First - The Purely Random Word Generator
-#v1.0 based on Zompist's Gen
-#import random
-
-#dropoff = "" # Dropoff determines how fast the power law declines. If you have consonants = ptkdgd then when calling a C, P will come up the most, followed by t, k, and so forth
-#monosyllables = "" #This tells the program how much of the output should be monosyllables
-
-print 'Settings:'
-
-def generate():
-    passval = 0
-    while passval == 0:
-        phones = raw_input('What phones would you like to use?')
-        if phones == 'help':
-            print '\nA phone is a distinct speech sound or gesture. Phones are usually either vowels or consonants, but your conlang can have different ones'
-            print 'Please try again'
-        else:
-            phones = list(phones)
-            passval = 1
-
-    passval = 0
-    while passval == 0:
-        syllable_types = raw_input('Enter the syllable types. Enter help if you need help.')
-        if syllable_types == 'help':
-            print 'Please seperate groupings with spaces. E.g CVVC for [consonant][vowedl][vowel][consonant] word \nAn example entry would be something like: CVVC CVC CCCVV VVCC'
-        else:
-            syllable_types = syllable_types.split()
-            passval = 1
-
-    passval = 0
-    while passval == 0:
-        categories = raw_input('Enter your categories. Enter help if you need help.')
-        if categories == 'help':
-            print 'Categories are phonological classes defined by enumeration. For example, you might define consonants like this: consonants=bcdfg. This means that every time a consonant is called ConGen will randomely select either b,c,d,f or g. The key here is that order determines probability. The default dropoff is 30%, which means that ConGen runs through each letter and has a 30 percent chance of stopping at every letter.'
-        else:
-            categories1 = categories.split()
-            categories2 = categories.something
-
-    passval = 0
-    while passval == 0:
-        dropoff = raw_input('Enter the dropoff value on a scale of 1-5 with 5 being the slowest. Enter help if you need help.')
-        if dropoff == 'help':
-            print 'Dropoff determines how fast the power law declines. If you have consonants = ptkdgd then when calling a C, P will come up the most, followed by t, k, and so forth'
-        else:
-            passval = 1
-generate()
-
-#First - The Purely Random Word Generator
 import random
 
 #Function to set the settings of the conlang
@@ -100,22 +53,32 @@ def set_values():
     print 'Categories: ' + str(categories)
     print 'Dropoff: ' + str(dropoff)
 
-    
+phones = ['c','v']
+syllable_types = ['cvc','vcc']
+categories = {'c' : ['w','i','l','l'], 'v' : ['c','h','r','i','s']}
+dropoff = 30
 
 #Function to generate a word list or table
 def generate():
-    wordlist = {}
+    syllable_dict = {}
+    wordlist = []
     wordnum = input('How many words would you like to generate?')
     wordnum = int(wordnum)
-    
-    for i in range(wordnum):
-        word = ''
-        syllable_type = random.choice(syllable_types)
-        syllable_type = list(syllable_type)
-        print syllable_type
-        
 
-           
+    #runs for every word
+    for i in range(wordnum):
+        word = []
+        
+        #runs for every syllable in the word
+        for x in range(random.randint(0,3)):
+            letters = []
+            syllable_type = random.choice(syllable_types)
+            syllable_type = list(syllable_type)
+            for j in syllable_type:
+                letters.append(random.choice(categories[j]))
+                word.append(''.join(letters))
+
+'''
 while True:
     print '\n1. Set values (word generation parameters)'
     print '2. Generate word list'
@@ -129,3 +92,7 @@ while True:
         generate()
     elif menuchoice == 3:
         exit()
+
+'''
+
+
